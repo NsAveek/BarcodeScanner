12c8ee8e11df36c80c94296b40ad77075e280a5e
Aveek <nsaveek@gmail.com>
2017-03-21 23:57:25 +0800

    initial commit

--- app/src/main/java/com/example/aveek/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/aveek/barcodescanner/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.aveek.barcodescanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private static final int RC_BARCODE_CAPTURE = 9001;
+    private static final String TAG = "BarcodeMain";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void switchActivity(View v) {
+        Intent intent = new Intent(this, BarcodeScannerActivity.class);
+        startActivityForResult(intent, RC_BARCODE_CAPTURE);
+//                super.onSaveInstanceState(savedInstanceState);
+        intent.putExtra(BarcodeScannerActivity.AutoFocus, true);
+        intent.putExtra(BarcodeScannerActivity.UseFlash, true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeScannerActivity.BarcodeObject);
+//                    statusMessage.setText("Barcode Success");
+//                    barcodeValue.setText(barcode.displayValue);
+                    Toast.makeText(this, barcode.displayValue, Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
+                } else {
+                    Toast.makeText(this, "Barcode Failed!", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, "No barcode captured, intent data is null");
+                }
+            } else {
+                /*statusMessage.setText(String.format("Barcode Error",
+                        CommonStatusCodes.getStatusCodeString(resultCode)));*/
+                Toast.makeText(this, "Barcode error!", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
---
+++ app/src/main/java/com/example/aveek/barcodescanner/model/BarcodeGraphicTracker.java
@@ -0,0 +1,72 @@
+package com.example.aveek.barcodescanner.model;
+
+/**
+ * Created by admin on 2/3/2017.
+ */
+
+
+import com.example.aveek.barcodescanner.ui.GraphicOverlay;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+/**
+ * Generic tracker which is used for tracking or reading a barcode (and can really be used for
+ * any type of item).  This is used to receive newly detected items, add a graphical representation
+ * to an overlay, update the graphics as the item changes, and remove the graphics when the item
+ * goes away.
+ */
+public class BarcodeGraphicTracker extends Tracker<Barcode> {
+    private GraphicOverlay<BarcodeGraphic> mOverlay;
+    private BarcodeGraphic mGraphic;
+    public static BarcodeDetectorListener mBarcodeDetectorListener;
+
+    BarcodeGraphicTracker(GraphicOverlay<BarcodeGraphic> overlay, BarcodeGraphic graphic) {
+        mOverlay = overlay;
+        mGraphic = graphic;
+    }
+
+    /**
+     * Start tracking the detected item instance within the item overlay.
+     */
+    @Override
+    public void onNewItem(int id, Barcode item) {
+        mGraphic.setId(id);
+        if(mBarcodeDetectorListener == null) return;
+        mBarcodeDetectorListener.onObjectDetected(item);
+        mBarcodeDetectorListener = null;
+    }
+
+    /**
+     * Update the position/characteristics of the item within the overlay.
+     */
+    @Override
+    public void onUpdate(Detector.Detections<Barcode> detectionResults, Barcode item) {
+        mOverlay.add(mGraphic);
+        mGraphic.updateItem(item);
+    }
+
+    /**
+     * Hide the graphic when the corresponding object was not detected.  This can happen for
+     * intermediate frames temporarily, for example if the object was momentarily blocked from
+     * view.
+     */
+    @Override
+    public void onMissing(Detector.Detections<Barcode> detectionResults) {
+        mOverlay.remove(mGraphic);
+    }
+
+    /**
+     * Called when the item is assumed to be gone for good. Remove the graphic annotation from
+     * the overlay.
+     */
+    @Override
+    public void onDone() {
+        mOverlay.remove(mGraphic);
+    }
+
+    public interface BarcodeDetectorListener{
+        //event call back
+        void onObjectDetected(Barcode data);
+    }
+}
